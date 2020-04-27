d07746f744e78e0169ae300179a321fc0dbd6b53
Simon Williams <simon@simon.com>
2020-04-27 15:45:39 +0800

    Clean up

--- Program.cs
+++ Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.IO;
-using System.Linq;
-using System.Text;
-using DfmExtractor.Commands;
-using DfmExtractor.Delphi;
-using DfmExtractor.Extensions;
+﻿using DfmExtractor.Commands;
+using System;
 
 namespace DfmExtractor
 {
@@ -14,24 +7,6 @@ namespace DfmExtractor
     {
         static void Main(string[] args)
         {
-            var lexer = new DfmLexer(File.OpenText(@"C:\CCare\Development\Misc\AboutBoxFrm.dfm"));
-            
-            var parser = new DfmParser(lexer);
-
-            var form = parser.ReadObject();
-
-            var forms = form.Children
-                .Flatten(a => a.Children)
-                .SelectMany(a => a.Properties)
-                .Where(a => a.Name.Contains("SQL"))
-                .ToArray();
-
-            foreach (var prop in forms)
-            {
-                //Console.WriteLine(prop.);
-                //Console.WriteLine(prop.Value);
-            }
-
             Console.WriteLine("Processing DFM files...");
 
             new UpdateDfmFolder().Execute();
